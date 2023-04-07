6ac886a7ac4d419caa8fade18c12769946cd0f1f
ScrexyScroo <107631447+ScrexyScroo@users.noreply.github.com>
2023-04-07 22:11:01 +0530

    Updated messages

--- dovi_check.py
+++ dovi_check.py
@@ -11,30 +11,38 @@ def getFilePath() -> str:
     if os.path.exists(videoFile):
         return videoFile
 
-    print('File does not exist')
+    print("File does not exist")
 
 
 # Python is bad i dont wanna restructure
 parsedMediaInfo = MediaInfo.parse(getFilePath())
 parsedMediaInfoJSON = json.loads(parsedMediaInfo.to_json())
-videoInfo = parsedMediaInfoJSON.get('tracks')[1]
-infoArr = ['format', 'format_profile', 'hdr_format', 'hdr_format_profile','hdr_format_settings', 'chroma_subsampling', 'codec_id']
+videoInfo = parsedMediaInfoJSON.get("tracks")[1]
+infoArr = [
+    "format",
+    "format_profile",
+    "hdr_format",
+    "hdr_format_profile",
+    "hdr_format_settings",
+    "chroma_subsampling",
+    "codec_id",
+]
 
 
 def getInfo(infoArr):
     for info in infoArr:
-        print(info, ':', videoInfo.get(info))
+        print(info, ":", videoInfo.get(info))
 
 
 def DoViCheck() -> bool:
-    if videoInfo.get('hdr_format') == 'Dolby Vision':
+    if videoInfo.get("hdr_format") == "Dolby Vision":
         return True
     else:
         return False
 
 
 def DoViProfile5Check() -> bool:
-    if videoInfo.get('hdr_format_profile') == 'dvhe.05':
+    if videoInfo.get("hdr_format_profile") == "dvhe.05":
         return True
 
 
@@ -46,22 +54,61 @@ def willThisRun() -> bool:
 
 
 def main():
-    print('\n')
+    print("\n")
     getInfo(infoArr)
 
     if willThisRun():
-        print('\n' + Back.LIGHTCYAN_EX + Fore.BLACK + ' BASED ' + Fore.LIGHTGREEN_EX + Back.BLACK + ' This video is DoVi profile 5 and is compatible with your TV' + Style.RESET_ALL + '\n')
-        if (parsedMediaInfoJSON.get('tracks')[0].get('file_extension') == 'mkv') or parsedMediaInfoJSON.get('tracks')[0].get('other_format') == 'Matroska':
-            print(Back.LIGHTYELLOW_EX + Fore.BLACK + ' Warning ' + Fore.YELLOW + Back.BLACK + ' This video is in mkv/Matroska container might cause issues in DoVi detection on your TV' + '\n' + Style.RESET_ALL)
-    
-    else if DoViCheck():
         print(
-            '\n' + Back.LIGHTWHITE_EX + Fore.BLACK + ' ISSUE ' + Fore.LIGHTRED_EX + Back.BLACK + ' This video file is not DoVi profile 5, might not work on TV' + Style.RESET_ALL + '\n')
+            "\n"
+            + Back.LIGHTCYAN_EX
+            + Fore.BLACK
+            + " BASED "
+            + Fore.LIGHTGREEN_EX
+            + Back.BLACK
+            + " This video is Dolby Vision profile 5 and is compatible with your LG TV"
+            + Style.RESET_ALL
+            + "\n"
+        )
+        if (
+            parsedMediaInfoJSON.get("tracks")[0].get("file_extension") == "mkv"
+        ) or parsedMediaInfoJSON.get("tracks")[0].get("other_format") == "Matroska":
+            print(
+                Back.LIGHTYELLOW_EX
+                + Fore.BLACK
+                + " Warning "
+                + Fore.YELLOW
+                + Back.BLACK
+                + " This video is in mkv/Matroska container and will cause HDR fallback if available on your LG TV"
+                + "\n"
+                + Style.RESET_ALL
+            )
+
+    if DoViCheck():
+        print(
+            "\n"
+            + Back.LIGHTWHITE_EX
+            + Fore.BLACK
+            + " ISSUE "
+            + Fore.LIGHTRED_EX
+            + Back.BLACK
+            + " This video file is not Dolby Vision profile 5, and will not work on your LG TV"
+            + Style.RESET_ALL
+            + "\n"
+        )
 
     else:
-         print(
-            '\n' + Back.LIGHTWHITE_EX + Fore.BLACK + ' Skipped ' + Fore.LIGHTRED_EX + Back.BLACK + ' This video file is not DoVi' + Style.RESET_ALL + '\n')
+        print(
+            "\n"
+            + Back.LIGHTWHITE_EX
+            + Fore.BLACK
+            + " Skipped "
+            + Fore.LIGHTRED_EX
+            + Back.BLACK
+            + " This video file is not Dolby Vision"
+            + Style.RESET_ALL
+            + "\n"
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
